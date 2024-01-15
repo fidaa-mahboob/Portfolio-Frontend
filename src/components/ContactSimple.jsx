efb28787759b7d4d30422e40176885267149cdc2
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2024-01-15 13:40:44 +0000

    made some updates

--- src/components/ContactSimple.jsx
+++ src/components/ContactSimple.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 
 const ContactSimple = () => {
     return (
-        <section className='py-4'>
-            <div>
-                <h1>Get In Touch</h1>
-                <p className="text-black text-lg pt-4">My email is fidaamahboob@gmail.com</p>
+        <section className="py-4">
+            <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+                <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-black">
+                    <strong>{"Let's Talk"}</strong>
+                </h1>
+
+           
+                <p className="pb-4 text-lg">
+            Contact me on 👉🏻{" "}
+                    <u className="underline decoration-red-500">
+              fidaamahboob@gmail.com
+                    </u>
+                </p>
             </div>
         </section>
-    )
+    );
 }
 
 export default ContactSimple
--- src/data/data.js
+++ src/data/data.js
@@ -4,8 +4,8 @@ export const data = () => {
             title: "Financial Record API MVP",
             subtitle: "Java / Springboot / AWS / Terraform",
             description:
-        "This is an end to end project that I am currently building for a client. In this project, I am building a Spring Boot REST API that will store complex financial data in a backend DynamoDB database in AWS. Click to find out more.",
-            image: "./weathersense.png",
+        "This is an end to end project that I am currently building for a local business. In this project, I am building a Spring Boot REST API that will store complex financial data in a backend DynamoDB database in AWS. Click to find out more.",
+            image: "./aws.png",
             link: "https://github.com/fidaa-mahboob/Financial-Record-API-MVP",
         },
         {
@@ -40,6 +40,6 @@ export const skills_data = () => {
         { name: "Docker", icon: "./docker.png" },
         { name: "MySQL", icon: "./mysql.png" },
         { name: "Python", icon: "./python.png" },
-        { name: "GraphQL", icon: "./mysql.png" },
+        { name: "GraphQL", icon: "./graphql-logo.png" },
     ];
 };
