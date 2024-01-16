ab726d2e26eeb74885d77cf8fae5000be0d3bc7a
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2024-01-16 15:23:15 +0000

    mid way to porting to gatsby

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import About from "./About";
+import Projects from "./Projects";
+import Skills from "./Skills";
+import Footer from "./Footer";
+import ContactSimple from "./ContactSimple";
+
+function App() {
+    return (
+        <main className="bg-[#F9F5F6]">
+            <About />
+            <Skills />
+            <Projects />
+            <ContactSimple />
+            <Footer />
+        </main>
+    );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
-const { nextui } = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,jsx,ts,tsx}",
+        "./src/components/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
         extend: {},
     },
-    darkMode: "class",
-    plugins: [nextui()],
+    plugins: [],
 };
